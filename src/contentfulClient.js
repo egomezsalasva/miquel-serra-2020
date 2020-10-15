ad476e3daf656b4f8b55fa1574687b51adf09110
Kike <kike@Kikes-MBP.home>
2020-10-15 20:12:24 +0200

    Test env

--- src/contentfulClient.js
+++ src/contentfulClient.js
@@ -1,5 +1,6 @@
 import * as contentful from 'contentful'
 
+// Test
 export const contentfulClient = contentful.createClient({
     space: process.env.REACT_APP_SPACE_ID,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
